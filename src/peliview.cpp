3c3ec786f8e728910cb91ce9e3ebba8e60e1f924
Juha Teurokoski <teurokoski.juha@gmail.com>
2013-12-15 13:38:54 +0200

    Yhteenvetolistaus päivitetty
    Muutettu tulostuksia sen mukaan mitä on selvitetty

--- src/peliview.cpp
+++ src/peliview.cpp
@@ -120,9 +120,17 @@ void PeliView::piirra_koneet() {
 			kirjoita_tekstia(peli.koneet[i].kutsutunnus, 30, listauskorkeus); 
 			
 			if (peli.koneet[i].reitti.size() == 0) {
-				kirjoita_tekstia(apuvalineet::tekstiksi(peli.koneet[i].selvityssuunta), 100, listauskorkeus);
+				if (peli.koneet[i].lahestymisselvitys) {
+					kirjoita_tekstia("ILS", 100, listauskorkeus);
+				} else if (peli.koneet[i].oikotie) {
+					kirjoita_tekstia(peli.koneet[i].ulosmenopiste.nimi, 100, listauskorkeus);
+				}
+					
+				if (!peli.koneet[i].lahestymisselvitys && !peli.koneet[i].oikotie) {
+					kirjoita_tekstia(apuvalineet::tekstiksi(peli.koneet[i].selvityssuunta), 100, listauskorkeus);
+				}
 			} else {
-				kirjoita_tekstia(peli.koneet[i].kohde.nimi, 100, listauskorkeus);
+				kirjoita_tekstia(peli.koneet[i].kohde.nimi, 100, listauskorkeus); 
 			}
 			
 			kirjoita_tekstia(apuvalineet::tekstiksi(peli.koneet[i].selvityskorkeus), 160, listauskorkeus);
