1d4e2a95b6e7fb5f1d993b510bb566b55fbc3898
Jaakko Haakana <jaakko@haakana.net>
2013-12-27 23:43:39 +0200

    Added Google mock library and first meaningful test. Fixed found bug in valikko.

--- src/valikko.cpp
+++ src/valikko.cpp
@@ -4,6 +4,18 @@
 #include "peliview.hpp"
 #include <iostream>
 
+template <typename Iter>
+Iter next_it(Iter iter)
+{
+	return ++iter;
+}
+
+template <typename Iter, typename Cont>
+bool is_last(Iter iter, const Cont& cont)
+{
+	return (iter != cont.end()) && (next_it(iter) == cont.end());
+}
+
 int valikko::aja() {
 	std::clog << "valikko::aja( )" << std::endl;
 
@@ -18,10 +30,11 @@ int valikko::aja() {
 		if (n == Ohjelma::NAPPI_ENTER) {
 			// Enter => lopetetaan valikko, palautetaan valinta.
 			return valinta->first;
-		} else if (n == Ohjelma::NAPPI_YLOS) {
+		} else if (n == Ohjelma::NAPPI_YLOS && valinta != kohdat.begin()) {
 			--valinta;
-		} else if (n == Ohjelma::NAPPI_ALAS) {
-			++valinta;
+		}
+		else if (n == Ohjelma::NAPPI_ALAS && !is_last(valinta, kohdat)) {
+			valinta++;
 		}
 	}
 }
--- test/atcradar_test/atcradar_test.cpp
+++ test/atcradar_test/atcradar_test.cpp
@@ -1,32 +1,85 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "lentokone.hpp"
 #include "peli.hpp"
+#include "peliview.hpp"
 #include "ohjelma.hpp"
+#include "piirtopinta.hpp"
 
-class OhjelmaMock : public IOhjelma
+// The following undef required due to some conflict with VS+SDL.' To be removed later...
+#ifdef _MSC_VER
+#undef main
+#endif
+
+using ::testing::Return;
+using ::testing::Sequence;
+
+class MockOhjelma : public IOhjelma
 {
-	virtual float sekunnit(bool nollaa = false) { return 0; }
-	virtual void odota(double ms = 1) { }
+public:
+	MOCK_METHOD1(sekunnit, float(bool nollaa));
+	MOCK_METHOD1(odota, void(double ms));
+	MOCK_METHOD0(odota_nappi, nappi());
+	MOCK_METHOD1(lue_nappi, bool(nappi n));
+	MOCK_METHOD0(tyhjenna_syote, void());
+	MOCK_METHOD3(onko_alueella, bool(const apuvalineet::piste& a, const apuvalineet::piste& b, double sade));
+	MOCK_METHOD0(anna_hiiri, apuvalineet::piste());
+	MOCK_METHOD0(lue_hiiri, bool());
+};
 
-	virtual nappi odota_nappi() { return nappi::NAPPI_MUU; }
-	virtual bool lue_nappi(nappi n) { return false; }
-	virtual void tyhjenna_syote() { }
-
-	virtual bool onko_alueella(const apuvalineet::piste& a, const apuvalineet::piste& b, double sade = 0.2) { return false; }
-	virtual apuvalineet::piste anna_hiiri() { apuvalineet::piste tmp; tmp.x = tmp.y = 0; return tmp; }
-	virtual bool lue_hiiri() { return false; }
+class MockPinta : public IPiirtoPinta
+{
+	MOCK_METHOD5(rectangleColor, void(Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Uint32 color));
+	MOCK_METHOD5(lineColor, void(Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Uint32 color));
+	MOCK_METHOD4(circleColor, void(Sint16 x1, Sint16 y1, Sint16 x2, Uint32 color));
+	MOCK_METHOD7(trigonColor, void(Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Sint16 x3, Sint16 y3, Uint32 color));
+	MOCK_METHOD4(piirra_kuva, void(SDL_Surface *kuva, int x, int y, bool keskikohta));
+	MOCK_METHOD4(kirjoita_tekstia, void(std::string teksti, int x, int y, bool aktiivinen));
+	MOCK_METHOD0(get_fontinkoko, int());
+	MOCK_METHOD0(flip, void());
 };
 
 TEST(LentokoneConstructor, LentokoneConstructor) {
 	Asetukset asetukset;
-	OhjelmaMock ohjelmaMock;
+	MockOhjelma ohjelmaMock;
     Kieli kieli;
     Peli peli(asetukset, ohjelmaMock, kieli);
 	apuvalineet::piste paikka;
 	paikka.x = 0;
 	paikka.y = 0;
 	lentokone kone(peli, ohjelmaMock, asetukset, "", paikka, 0, 0, 0, 0, false);
+}
+
+TEST(ValikkoTests, ValikkoBounds) {
+	Asetukset asetukset;
+	MockOhjelma ohjelmaMock;
+	MockPinta pinta;
+	Kieli kieli;
+	Peli peli(asetukset, ohjelmaMock, kieli);
+	PeliView peliview(pinta, asetukset, peli, ohjelmaMock, kieli);
+	valikko v(ohjelmaMock, peliview);
+
+	IOhjelma::nappi nappisequence[] = {
+		IOhjelma::nappi::NAPPI_ALAS,
+		IOhjelma::nappi::NAPPI_ALAS,
+		IOhjelma::nappi::NAPPI_ALAS,
+		IOhjelma::nappi::NAPPI_YLOS,
+		IOhjelma::nappi::NAPPI_YLOS,
+		IOhjelma::nappi::NAPPI_YLOS,
+		IOhjelma::nappi::NAPPI_ENTER,
+	};
+
+	Sequence seq;
+	for (int i = 0; i < 7; i++)
+	{
+		EXPECT_CALL(ohjelmaMock, odota_nappi()).InSequence(seq).WillOnce(Return(nappisequence[i]));
+	}
+
+	v.lisaa_kohta(0, "dummy1");
+	v.lisaa_kohta(1, "dummy1");
+
+	v.aja();
 }
 
 int main(int argc, char* argv[])
