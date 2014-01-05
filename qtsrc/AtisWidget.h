7957bade2e31b75813d06e3267fba6df4cc8f96c
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-01-05 20:40:25 +0200

    Fixattu QIntValidator

--- qtsrc/AtisWidget.h
+++ qtsrc/AtisWidget.h
@@ -10,8 +10,7 @@
 #include <QValidator>
 #include <iostream>
 
-class AtisView : public QWidget
-{
+class AtisView : public QWidget {
 	Q_OBJECT
 public:
     AtisView(QWidget* parent = 0) : QWidget(parent) {
@@ -24,13 +23,9 @@ public:
 		okButton = new QPushButton("OK", this);
 		okButton->move(80, 85);
 
-        QValidator* validate_dep = new QIntValidator(01, 36, this);
-        QValidator* validate_ldg = new QIntValidator(01, 36, this);
-        QValidator* validate_TL = new QIntValidator(40, 70, this);
-        QValidator* validate_TA = new QIntValidator(3000, 18000, this);
-
         departRWY = new QLineEdit("", this);
         departRWY->move(120, 120);
+        QIntValidator* validate_dep = new QIntValidator(1, 36, departRWY);
         departRWY->setValidator(validate_dep);
 
         departure = new QLabel("Lähtöbaana", this);
@@ -38,6 +33,7 @@ public:
 
         landingRWY = new QLineEdit("", this);
         landingRWY->move(120, 140);
+        QIntValidator* validate_ldg = new QIntValidator(01, 36, landingRWY);
         landingRWY->setValidator(validate_ldg);
 
         landing = new QLabel("Laskubaana", this);
@@ -45,6 +41,7 @@ public:
 
         TA = new QLineEdit("", this);
         TA->move(120, 160);
+        QIntValidator* validate_TA = new QIntValidator(3000, 18000, TA);
         TA->setValidator(validate_TA);
 
         transitionLevel = new QLabel("Siirtokorkeus", this);
@@ -52,6 +49,7 @@ public:
 
         TL = new QLineEdit("", this);
         TL->move(120, 180);
+        QIntValidator* validate_TL = new QIntValidator(30, 180, TL);
         TL->setValidator(validate_TL);
 
         transitionAltitude = new QLabel("Siirtopinta", this);
