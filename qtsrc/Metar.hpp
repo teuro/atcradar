30aa5cf81168905899f9b997b86e36ba0651c3c5
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-01-06 18:27:16 +0200

    Metariin lisätty kentät.

--- qtsrc/Metar.hpp
+++ qtsrc/Metar.hpp
@@ -8,15 +8,25 @@ class Metar {
     int tuuli;
     int voimakkuus;
     double paine;
+    int nakyvyys;
+    int lampotila;
+    int ilmankosteus;
+    int kastepiste;
+    std::string pilvet;
 public:
     Metar () {
-        tuuli       = apuvalineet::pyorista(apuvalineet::arvo_luku(3, 23), 5);
-        voimakkuus  = apuvalineet::arvo_luku(0, 20);
-        paine       = apuvalineet::arvo_luku(940, 1060);
+        tuuli           = apuvalineet::pyorista(apuvalineet::arvo_luku(3, 23), 5);
+        voimakkuus      = apuvalineet::arvo_luku(0, 20);
+        paine           = apuvalineet::arvo_luku(940, 1060);
+        nakyvyys        = apuvalineet::pyorista(apuvalineet::arvo_luku(200, 9999), 100);
+        lampotila       = apuvalineet::arvo_luku(-30, 60);
+        ilmankosteus    = apuvalineet::arvo_luku(50, 100);
+        kastepiste      = (lampotila - (100 - ilmankosteus)) / 5;
+        pilvet          = "BKN2700 SCT6800 OVC1100";
     }
 
     QString getMessage() {
-        return QString::fromStdString(apuvalineet::muuta_pituus(apuvalineet::tekstiksi(tuuli), 3) + apuvalineet::muuta_pituus(apuvalineet::tekstiksi(voimakkuus), 2) + "KT Q" + apuvalineet::tekstiksi(paine));
+        return QString::fromStdString("EFRO " + apuvalineet::muuta_pituus(apuvalineet::tekstiksi(tuuli), 3) + apuvalineet::muuta_pituus(apuvalineet::tekstiksi(voimakkuus), 2) + "KT Q" + apuvalineet::tekstiksi(paine) + " " + apuvalineet::tekstiksi(nakyvyys) + " " + apuvalineet::tekstiksi(lampotila) + "/" + apuvalineet::tekstiksi(kastepiste) + " " + pilvet);
     }
 };
 
