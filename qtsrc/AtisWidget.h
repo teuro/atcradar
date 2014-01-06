4400047b40586ea2140e57112d616e0a306cb36f
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-01-06 15:07:26 +0200

    Syöttökentille oma rutiini

--- qtsrc/AtisWidget.h
+++ qtsrc/AtisWidget.h
@@ -19,46 +19,30 @@ public:
 		title = new QLabel("ATIS Valinta", this);
         title->setGeometry(0, 0, 150, 30);
 
-        MetarLabel = new QLabel(metar.getMessage(), this);
-        MetarLabel->setGeometry(0, 0, 600, 10);
+        metarLabel = new QLabel(metar.getMessage(), this);
+        metarLabel->setGeometry(0, 0, 600, 10);
+
+        drawInputField("Lähtökiitotie", 100, 60, 1, 36);
+        drawInputField("Laskukiitotie", 100, 80, 1, 36);
+        drawInputField("Siirtokorkeus", 100, 100, 3000, 18000);
+        drawInputField("Siirtopinta", 100, 120, 30, 220);
 
 		okButton = new QPushButton("OK", this);
-		okButton->move(80, 85);
-
-        departRWY = new QLineEdit("", this);
-        departRWY->move(120, 120);
-        QIntValidator* validate_dep = new QIntValidator(1, 36, departRWY);
-        departRWY->setValidator(validate_dep);
-
-        departure = new QLabel("Lähtöbaana", this);
-        departure->setGeometry(50, 120, 60, 10);
-
-        landingRWY = new QLineEdit("", this);
-        landingRWY->move(120, 140);
-        QIntValidator* validate_ldg = new QIntValidator(01, 36, landingRWY);
-        landingRWY->setValidator(validate_ldg);
-
-        landing = new QLabel("Laskubaana", this);
-        landing->setGeometry(50, 140, 60, 10);
-
-        TA = new QLineEdit("", this);
-        TA->move(120, 160);
-        QIntValidator* validate_TA = new QIntValidator(3000, 18000, TA);
-        TA->setValidator(validate_TA);
-
-        transitionLevel = new QLabel("Siirtokorkeus", this);
-        transitionLevel->setGeometry(50, 160, 60, 10);
-
-        TL = new QLineEdit("", this);
-        TL->move(120, 180);
-        QIntValidator* validate_TL = new QIntValidator(30, 180, TL);
-        TL->setValidator(validate_TL);
-
-        transitionAltitude = new QLabel("Siirtopinta", this);
-        transitionAltitude->setGeometry(50, 180, 60, 10);
+        okButton->move(100, 160);
 
         connect(okButton, SIGNAL(clicked()), this, SLOT(OnOkPressed()));
 	}
+
+    void drawInputField(QString name, int x, int y, int minimum, int maximum) {
+        inputField = new QLineEdit("", this);
+        inputField->move(x, y);
+
+        QIntValidator* tmp = new QIntValidator(minimum, maximum, inputField);
+        inputField->setValidator(tmp);
+
+        inputLabel = new QLabel(name, this);
+        inputLabel->setGeometry(x-80, y-0, 200, 20);
+    }
 
 	public slots:
     void OnOkPressed() {
@@ -70,19 +54,14 @@ signals:
 	void atisDone();
 
 private:
+
 	QLabel* title;
-    QLabel* departure;
-    QLabel* landing;
-    QLabel* transitionAltitude;
-    QLabel* transitionLevel;
-    QLabel* MetarLabel;
+    QLabel* inputLabel;
+    QLabel* metarLabel;
 
 	QPushButton* okButton;
 
-    QLineEdit* departRWY;
-    QLineEdit* landingRWY;
-    QLineEdit* TA;
-    QLineEdit* TL;
+    QLineEdit* inputField;
 };
 
 #endif
