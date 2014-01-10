1383d419d9a55cd8fb4e3b2f08150de0e9a815e1
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-01-10 20:54:16 +0200

    Selvityksille omat kentät

--- qtsrc/PeliWidget.h
+++ qtsrc/PeliWidget.h
@@ -5,12 +5,13 @@
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QPushButton>
 #include <QMouseEvent>
+#include <QLineEdit>
+#include <QValidator>
 #include <QPainter>
 #include <QTimer>
 #include <QTime>
 
 #include "PeliView.h"
-#include "peli.hpp"
 #include "pelicontroller.hpp"
 #include "QpiirtoPinta.h"
 
@@ -30,7 +31,27 @@ public:
         aika = new QTime();
 
         // To get mouse events continuously even when button is not pressed
-        setMouseTracking(true) ;
+        setMouseTracking(true);
+
+        addInputField("Suunta", 100, 20, 0, 360);
+        addInputField("Nopeus", 100, 40, 160, 440);
+        addInputField("Korkeus", 100, 60, 2000, 24000);
+
+        okButton = new QPushButton("OK", this);
+        okButton->move(100, 80);
+
+        connect(okButton, SIGNAL(clicked()), this, SLOT(OnOkPressed()));
+    }
+
+    void addInputField(QString name, int x, int y, int minimum, int maximum) {
+        inputFields.push_back(new QLineEdit("", this));
+        inputFields.back()->move(x, y);
+
+        QIntValidator* tmp = new QIntValidator(minimum, maximum, inputFields.back());
+        inputFields.back()->setValidator(tmp);
+
+        inputLabel = new QLabel(name, this);
+        inputLabel->setGeometry(x-80, y-0, 200, 20);
     }
 
 public slots:
@@ -38,6 +59,10 @@ public slots:
         peliController.kasittele_aikaa(frameMs / 1000.0);
         peliController.ota_aika(aika->elapsed());
         update();
+    }
+
+    void OnOkPressed() {
+
     }
 
     // Redraw the view completely
@@ -48,9 +73,10 @@ public slots:
     }
 
     // Mouse button up
-    void mouseReleaseEvent(QMouseEvent * e) {
+    void mouseReleaseEvent(QMouseEvent* e) {
         QPoint pos = e->pos();
         peliController.kasittele_hiiren_nappi(apuvalineet::piste(pos.x(), pos.y()));
+
         update();
     }
 
@@ -66,6 +92,14 @@ private:
     QTime* aika;
     PeliView& peliView;
     PeliController& peliController;
+
+    QLabel* inputLabel;
+    QLabel* metarLabel;
+    QLabel* error;
+
+    QPushButton* okButton;
+
+    std::vector <QLineEdit*> inputFields;
 };
 
 
