93dbe19e8f01c4cd04f0c56c46533c6f84d38b85
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-01-11 18:41:56 +0200

    Tasonvalinta muokattu

--- qtsrc/levelmenu.h
+++ qtsrc/levelmenu.h
@@ -6,6 +6,11 @@
 #include <QtWidgets/QSlider>
 #include <QtWidgets/QWidget>
 #include <QtWidgets/QPushButton>
+#include <QString>
+#include <fstream>
+#include <vector>
+#include <stdexcept>
+#include "apuvalineet.hpp"
 
 class LevelMenu : public QWidget
 {
@@ -18,15 +23,15 @@ public:
         slider->setMaximum(4);
 
 		title = new QLabel("Valitse vaikeustaso: ", this);
-		title->setGeometry(50, 20, 150, 30);
+        title->setGeometry(50, 20, 150, 30);
 
-		levelLabel = new QLabel("0", this);
-		levelLabel->setGeometry(230, 50, 20, 30);
+        levelLabel = new QLabel("0", this);
+        levelLabel->setGeometry(230, 50, 20, 30);
 
 		okButton = new QPushButton("OK", this);
 		okButton->move(80, 85);
 
-		connect(slider, SIGNAL(valueChanged(int)), levelLabel, SLOT(setNum(int)));
+        connect(slider, SIGNAL(valueChanged(int)), levelLabel, SLOT(setNum(int)));
 		connect(okButton, SIGNAL(clicked()), this, SLOT(OnOkPressed()));
 	}
 
@@ -39,11 +44,11 @@ public:
 	}
 
 public slots:
-	void OnOkPressed()
-	{
+    void OnOkPressed()
+    {
         emit levelSelected(slider->value());
 		//close();
-	}
+    }
 
 signals:
 	void levelSelected(int level);
@@ -52,7 +57,27 @@ private:
 	QLabel* title;
 	QSlider* slider;
 	QLabel* levelLabel;
-	QPushButton* okButton;
+    QLabel* ruutu;
+    QPushButton* okButton;
+
+    QString getInfo(int level) {
+        std::ifstream sisaan("data/tasot.txt", std::ios::in);
+
+        if (!sisaan) {
+            throw std::runtime_error("Tiedosto data/tasot.txt ei aukea");
+        }
+
+        std::string teksti;
+
+        while (std::getline(sisaan, teksti)) {
+            std::vector <std::string> asiat = apuvalineet::pilko_rivi(teksti, "|");
+            if (apuvalineet::luvuksi<int>(asiat[0]) == level) {
+                std::clog << asiat[1] << std::endl;
+                sisaan.close();
+                return QString::fromStdString(asiat[1]);
+            }
+        }
+    }
 };
 
 #endif
