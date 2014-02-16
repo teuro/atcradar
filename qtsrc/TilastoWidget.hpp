cf120b8d55e40d97c8177c664eaf7edab457e5b1
Juha Teurokoski <teurokoski.juha@gmail.com>
2014-02-16 16:29:50 +0200

    Tilastot kuntoon

--- qtsrc/TilastoWidget.hpp
+++ qtsrc/TilastoWidget.hpp
@@ -52,7 +52,7 @@ public slots:
             tilastot->setItem(i, 0, new QTableWidgetItem(QString::fromStdString(peli.ajat[i].tunnus)));
             tilastot->setItem(i, 1, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].sisaan))));
             tilastot->setItem(i, 2, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].pois))));
-            tilastot->setItem(i, 3, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].pois - peli.ajat[i].pois))));
+            tilastot->setItem(i, 3, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].pois - peli.ajat[i].sisaan))));
             tilastot->setItem(i, 4, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].selvitykset))));
             tilastot->setItem(i, 5, new QTableWidgetItem(QString::fromStdString(apuvalineet::tekstiksi(peli.ajat[i].pisteet))));
         }
